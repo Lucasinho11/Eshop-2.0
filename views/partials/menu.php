b502cc36af98a155125d76869774c842c6d32dcf
Lucas <lucas.lubasinski11@gmail.com>
2020-06-19 23:57:03 +0200

    burger

--- views/partials/menu.php
+++ views/partials/menu.php
@@ -26,9 +26,34 @@
                 <a href="index.php?p=questions">Questions</a> 
             </div> 
             <div class="icones">
-                <a href="index.php?p=user"><i class="fas fa-user"></i></a><br>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-shopping-cart"></i>
+                <div class="icone-profile">
+                    <a href="index.php?p=user"><i class="fas fa-user"></i></a><br>
+                </div>
+
+                <div class="fav"
+                    ><i class="fas fa-star"></i>
+                </div>
+                <div class="icone-cart">
+                    <i class="fas fa-shopping-cart"></i>
+                </div>
+                <div class="burger">
+                    <input id="burger" type="checkbox" />
+
+                    <label class="span-burger"for="burger">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </label> 
+                    <div class="burger-menu">
+                        <ul>
+                        <?php foreach($parentsCategories as $category):?>
+                            <li><a href="index.php?p=category&category_id=<?= $category['id'] ?>">
+                            <?= $category['name'] ?></li>
+                            </a><br>
+                        <?php endforeach;?>
+                        </ul>  
+                    </div>
+                </div>
             </div>
             
         </nav>
